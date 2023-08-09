d346941c85043b5d97f789c4e9e43059326d6d4b
kejiAK2022 <124058553+kejiAK2022@users.noreply.github.com>
2023-08-09 19:23:07 +0800

    Add files via upload

---
+++ status/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '服务器状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2255402-3242de0778bdaef05d110b98',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+  //      text: '首页',
+  //      url: 'https://blog.9az.ren/'
+    },
+  ],
+};
